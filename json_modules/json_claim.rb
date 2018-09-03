efb1a398c2128c45c60b7c8fbccf4bf81e4c7e8d
ian-mcwilliams <ian.mcwilliams@f3mmedia.com>
2018-09-03 11:08:38 +0100

    move add json element functionality to reusable location

--- json_modules/json_claim.rb
+++ json_modules/json_claim.rb
@@ -4,21 +4,17 @@ module JsonClaim
 	include JsonElements
 
 	def self.build_json_claim(claim)
-		json_claim = {}.to_json
-		json_claim = add_json_claim_element(json_claim, JsonElements.amount)
-		json_claim = add_json_claim_element(json_claim, JsonElements.payment)
-		json_claim = add_json_claim_element(json_claim, JsonElements.timeline)
-		json_claim = add_json_claim_element(json_claim, JsonElements.interest)
-		json_claim = add_json_claim_element(json_claim, JsonElements.claimants)
-		json_claim = add_json_claim_element(json_claim, JsonElements.defendants)
-		json_claim = add_json_claim_element(json_claim, JsonElements.reason)
-		json_claim = add_json_claim_element(json_claim, JsonElements.timeline)
-		json_claim = add_json_claim_element(json_claim, JsonElements.evidence)
-		json_claim = add_json_claim_element(json_claim, JsonElements.fee_amount_in_pennies)
-	end
-
-	def self.add_json_claim_element(json_claim, new_element)
-		JSON.parse(json_claim).merge(JSON.parse(new_element)).to_json
+		json_claim = JsonElements.add_json_element({}.to_json, JsonElements.amount)
+		json_claim = JsonElements.add_json_element(json_claim, JsonElements.payment)
+		json_claim = JsonElements.add_json_element(json_claim, JsonElements.timeline)
+		json_claim = JsonElements.add_json_element(json_claim, JsonElements.interest)
+		json_claim = JsonElements.add_json_element(json_claim, JsonElements.claimants)
+		json_claim = JsonElements.add_json_element(json_claim, JsonElements.defendants)
+		json_claim = JsonElements.add_json_element(json_claim, JsonElements.reason)
+		json_claim = JsonElements.add_json_element(json_claim, JsonElements.timeline)
+		json_claim = JsonElements.add_json_element(json_claim, JsonElements.evidence)
+		json_claim = JsonElements.add_json_element(json_claim, JsonElements.fee_amount_in_pennies)
+		json_claim
 	end
 
 end
--- json_modules/json_elements.rb
+++ json_modules/json_elements.rb
@@ -1,5 +1,9 @@
 module JsonElements
 
+	def self.add_json_element(json, new_element)
+		JSON.parse(json).merge(JSON.parse(new_element)).to_json
+	end
+
 	def self.amount
     	{
 			"amount": {
