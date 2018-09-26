17bbbc67e6e9ae712223da42dccdc5e3e778a663
ian-mcwilliams <ian.mcwilliams@f3mmedia.com>
2018-09-26 15:12:41 +0100

    update to now work for aat

--- generate_cmc_records.rb
+++ generate_cmc_records.rb
@@ -37,9 +37,9 @@ module GenerateCmcRecords
     output_message("###   ITERATION #{iteration_id}   ###\n\n") if iteration_id
 
     claim_api_call_response = claim_api_call(target_env, args[:claim], env_prefix, claimant_id, claimant_session_id)
+    claim_no = JSON.parse(claim_api_call_response.body)['referenceNumber']
 
     if target_env == :local
-      claim_no = JSON.parse(claim_api_call_response.body)['referenceNumber']
       set_admissions_to_true(claim_no, args[:path_to_integration_tests])
       output_message("admissions set to TRUE for claim_no '#{claim_no}'\n\n")
     end
@@ -68,7 +68,7 @@ module GenerateCmcRecords
   def self.claim_api_call(target_env, claim, env_prefix, claimant_id, claimant_session_id)
     json_claim = JsonClaim.build_json_claim(claim)
     claim_url = "#{env_prefix}/claims/#{claimant_id}"
-    response = api_call(target_env, 'claim', claim_url, {session_id: claimant_session_id, body: json_claim})
+    response = api_call(target_env, 'claim', claim_url, {session_id: claimant_session_id, body: json_claim, env_prefix: env_prefix})
     raise('api call failure') unless response.class == Net::HTTPOK
     response
   end
@@ -80,7 +80,7 @@ module GenerateCmcRecords
 
   def self.link_defendant_api_call(target_env, env_prefix, claim_no, defendant_id)
     link_defendant_url = "#{env_prefix}/testing-support/claims/#{claim_no}/defendant/#{defendant_id}"
-    response = api_call(target_env, 'link_defendant', link_defendant_url)
+    response = api_call(target_env, 'link_defendant', link_defendant_url, {env_prefix: env_prefix})
     raise('api call failure') unless response.class == Net::HTTPOK
     response
   end
@@ -88,7 +88,7 @@ module GenerateCmcRecords
   def self.defendant_response_api_call(target_env, defendant_response, env_prefix, external_id, defendant_id, defendant_session_id)
     json_defendant_response = JsonDefendantResponse.build_json_defendant_response(defendant_response)
     defendant_response_url = "#{env_prefix}/responses/claim/#{external_id}/defendant/#{defendant_id}"
-    args = {session_id: defendant_session_id, body: json_defendant_response}
+    args = {session_id: defendant_session_id, body: json_defendant_response, env_prefix: env_prefix}
     response = api_call(target_env, 'defendant_response', defendant_response_url, args)
     raise('api call failure') unless response.class == Net::HTTPOK
     response
@@ -97,7 +97,7 @@ module GenerateCmcRecords
   def self.claimant_response_api_call(target_env, claimant_response, env_prefix, external_id, claimant_id, claimant_session_id)
     json_claimant_response = JsonClaimantResponse.build_json_claimant_response(claimant_response)
     claimant_response_url = "http://#{env_prefix}/responses/#{external_id}/claimant/#{claimant_id}"
-    args = {session_id: claimant_session_id, body: json_claimant_response}
+    args = {session_id: claimant_session_id, body: json_claimant_response, env_prefix: env_prefix}
     response = api_call(target_env, 'claimant_response', claimant_response_url, args)
     raise('api call failure') unless response.class == Net::HTTPOK
     response
@@ -115,7 +115,7 @@ module GenerateCmcRecords
     response = nil
     case target_env
     when :aat
-      Net::HTTP.new(url, nil, 'proxyout.reform.hmcts.net', 8080).start do |http|
+      Net::HTTP.new(args[:env_prefix], nil, 'proxyout.reform.hmcts.net', 8080).start do |http|
         response = api_request(http, req, type)
       end
     when :local, :demo
