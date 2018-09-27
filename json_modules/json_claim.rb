1c3c626e2ea3e00c8d34c48193e2e2ad63f17e16
ian-mcwilliams <ian.mcwilliams@f3mmedia.com>
2018-09-27 09:41:32 +0100

    move claim json methods to specific module for them

--- json_modules/json_claim.rb
+++ json_modules/json_claim.rb
@@ -1,19 +1,20 @@
 require_relative 'json_elements'
+require_relative 'json_claim_elements'
 
 module JsonClaim
 	include JsonElements
 
 	def self.build_json_claim(claim)
-		json_claim = JsonElements.add_json_element({}.to_json, JsonElements.amount)
-		json_claim = JsonElements.add_json_element(json_claim, JsonElements.payment)
-		json_claim = JsonElements.add_json_element(json_claim, JsonElements.timeline)
-		json_claim = JsonElements.add_json_element(json_claim, JsonElements.interest)
-		json_claim = JsonElements.add_json_element(json_claim, JsonElements.claimants)
-		json_claim = JsonElements.add_json_element(json_claim, JsonElements.defendants)
-		json_claim = JsonElements.add_json_element(json_claim, JsonElements.reason)
-		json_claim = JsonElements.add_json_element(json_claim, JsonElements.timeline)
-		json_claim = JsonElements.add_json_element(json_claim, JsonElements.evidence)
-		json_claim = JsonElements.add_json_element(json_claim, JsonElements.fee_amount_in_pennies)
+		json_claim = JsonElements.add_json_element({}.to_json, JsonClaimElements.amount)
+		json_claim = JsonElements.add_json_element(json_claim, JsonClaimElements.payment)
+		json_claim = JsonElements.add_json_element(json_claim, JsonClaimElements.timeline)
+		json_claim = JsonElements.add_json_element(json_claim, JsonClaimElements.interest)
+		json_claim = JsonElements.add_json_element(json_claim, JsonClaimElements.claimants)
+		json_claim = JsonElements.add_json_element(json_claim, JsonClaimElements.defendants)
+		json_claim = JsonElements.add_json_element(json_claim, JsonClaimElements.reason)
+		json_claim = JsonElements.add_json_element(json_claim, JsonClaimElements.timeline)
+		json_claim = JsonElements.add_json_element(json_claim, JsonClaimElements.evidence)
+		json_claim = JsonElements.add_json_element(json_claim, JsonClaimElements.fee_amount_in_pennies)
 		json_claim
 	end
 
