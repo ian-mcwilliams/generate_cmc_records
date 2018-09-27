519dce52e7ce07a17839253eda61017b45c568cc
ian-mcwilliams <ian.mcwilliams@f3mmedia.com>
2018-09-27 08:35:23 +0100

    initial draft of claimant response file

---
+++ json_modules/json_claimant_response.rb
@@ -0,0 +1,20 @@
+module JsonClaimantResponse
+  def self.build_json_claimant_response(claimant_response)
+    case claimant_response
+    when :accept_repayment_with_ccj
+      json_accept_repayment_with_ccj
+    end
+  end
+
+  def self.json_accept_repayment_with_ccj
+    {
+      "type": "acceptation",
+      "amountPaid": 10,
+      "claimantPaymentIntention": {
+        "paymentOption": "BY_SPECIFIED_DATE",
+        "paymentDate": "2018-09-27"
+      },
+      "formaliseOption": "CCJ"
+    }
+  end
+end
